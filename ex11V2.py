ac5c1a76c34915c12a73f0fc62c4e53f9af57f41
barrie Woolley <jazzerbazza@icloud.com>
2023-03-13 18:11:49 +0000

    while loops commit

---
+++ ex11V2.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+Belgium = 'Belgium,10445852,Brussels,737966,Europe,1830,Euro,Catholicism,Dutch,French,German'
+
+
+#Print a) a line of hyphens the same length of the Belgium string
+print(
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ while_loops.py
@@ -0,0 +1,16 @@
+# while example
+#ine = None
+
+#while line != 'done':
+#    line = input("Type done to complete")
+#    print("<", line, ">")
+
+#import sys
+#for arg in sys.argv:
+#   print("Cmd line argument:", arg)
+
+
+ import sys
+ for idx. arg in enumerate(sys.argv):
+     print("index:", idx, "argument:", arg)
+
